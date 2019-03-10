b924a2fc73cc4a2c062a6c0dfb2d57367c9ef0ff
Tomasz Jureczkin <n3mszn@gmail.com>
2019-03-10 14:02:18 +0100

    Structure updated

---
+++ src/Pages/FindDetail/findDetail.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class FindDetail extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p> Lost Detail </p>
+        
+      </div>
+    );
+  }
+}
+
+export default FindDetail;
---
+++ src/Pages/LostDetail/lostDetail.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class LostDetail extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p> Lost Detail </p>
+        
+      </div>
+    );
+  }
+}
+
+export default LostDetail;
