a335c234e55fc2207e9aa7cade620eddf1048268
Tomasz Jureczkin <n3mszn@gmail.com>
2019-03-10 13:52:19 +0100

    Structure of folder

---
+++ src/Layout/footer/footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p> Footer </p>
+        
+      </div>
+    );
+  }
+}
+
+export default Footer;
