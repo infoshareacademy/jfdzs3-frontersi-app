ca825606caab2cec71b10f7eb4dca2b0106bfcc5
n3mszn <n3mszn@pc40.home>
2019-03-23 20:50:24 +0100

    some navbar changes

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Logo from '../../img/logo.png';
 import find from '../../img/Header/2.png';
+import text from '../../img/Header/1.png';
 import lost from '../../img/Header/3.png';
 import favourite from '../../img/Header/star_favourite.png';
 import './navbar.css';
@@ -18,10 +19,14 @@ class Navbar extends Component {
         <div className="container d-flex fixed-top flex-column flex-wrap flex-lg-row align-items-center bg-white">
         <a href="#" className="my-1 mr-md-auto"><img style={logotype} src={Logo} class="header-logo" alt="logo"/></a>
         <nav className="my-2 my-md-0 mr-md-3">
-        <a className=" p-2 text-dark main-nav-size"><img className="img-width" src={find} ></img></a>
-        <a className="p-2 text-dark main-nav-size"><img className="img-width" src={lost} ></img></a>
+        <div className="nav-wrapper">
+        <a className=" p-2 text-dark main-nav-size nav-item"><img className="img-width" src={find} ></img></a>
+        <a className="p-2 text-dark main-nav-size nav-item"><img className="img-width" src={lost} ></img></a>
+        </div>
+        <a className="p-2 text-dark main-nav-size nav-item nav-item-third"><img className="img-width" src={text} ></img></a>
+      
         </nav>
-        <a className="btn orange-btn main-nav-size"><img className="img-width" src={favourite} ></img></a>
+        <a className="btn orange-btn main-nav-size"><img className="img-fav-width" src={favourite} ></img></a>
         </div>
 
 
