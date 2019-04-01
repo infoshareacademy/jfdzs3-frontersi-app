434e806f3fe6459763fa770226b678d77dd928b9
Sonia <sonia@przenioslo.eu>
2019-04-01 22:55:42 +0200

    Modified css in layout

--- src/Pages/FindDetail/findDetail.js
+++ src/Pages/FindDetail/findDetail.js
@@ -6,7 +6,17 @@ class FindDetail extends Component {
     return (
       <div className="App">
         <div className='lost_detail_container'>
-        <p> Lost Detail </p>
+        <p> 
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sit amet massa sed sapien varius elementum quis vitae orci. Duis augue ante, convallis dignissim ligula non, vulputate pellentesque odio. Ut nec justo massa. In non justo blandit, commodo felis in, pellentesque mauris. Integer tellus nisi, ultricies non suscipit non, eleifend quis ante. Donec maximus diam sed enim tristique varius. Vestibulum auctor, enim eu sollicitudin tempor, tellus lectus feugiat ipsum, in consequat justo leo et mauris. Donec cursus convallis luctus. Morbi congue vestibulum accumsan. Nullam a ornare nibh. Pellentesque egestas orci eget ex eleifend, molestie commodo turpis bibendum. Maecenas nec commodo mauris. In id aliquet tortor.
+
+Ut semper odio at libero imperdiet tincidunt. Quisque velit diam, efficitur nec ex in, porta tincidunt urna. Vestibulum vitae fermentum tortor. Aliquam purus nisl, euismod at faucibus in, aliquet nec diam. Duis varius in felis et consequat. Donec at sem sit amet urna vulputate facilisis vitae at elit. Suspendisse eget neque vitae orci elementum pharetra id pulvinar nisi.
+
+Morbi id mauris lorem. Integer felis felis, elementum ac pharetra a, pharetra at nisl. Proin odio arcu, laoreet vel quam et, viverra interdum ligula. Pellentesque cursus dui tortor, id dictum nibh bibendum posuere. Mauris mauris purus, sodales sed euismod vel, accumsan vitae libero. Nullam enim neque, pretium a aliquet eu, posuere a metus. Curabitur nec laoreet ex, quis accumsan magna. Aenean malesuada enim ut velit porttitor, non laoreet odio sagittis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Praesent ullamcorper malesuada bibendum.
+
+Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent bibendum, nibh nec dapibus tempor, tellus sapien posuere sapien, non elementum nibh tortor vitae justo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque non mollis orci. Etiam et ante tempus, egestas risus sed, porta quam. Ut id tristique dolor. Donec euismod, lacus nec gravida semper, velit nunc rhoncus lacus, et egestas tellus ipsum ut quam. Nulla facilisi. Nunc dapibus et erat eu lacinia.
+
+Aenean condimentum, tortor et facilisis pharetra, ligula purus auctor ipsum, id ornare tellus urna quis orci. Vestibulum a nulla consequat, finibus risus eu, tincidunt ligula. Donec dui turpis, tincidunt ac erat ut, tristique porta ex. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nullam nec neque purus. Aenean consequat turpis tortor, sed bibendum nibh euismod eu. Nulla facilisi. Aenean volutpat metus non pretium dictum. Nunc ac arcu sed lacus sagittis malesuada nec ac ligula. Phasellus consectetur non magna vitae ornare. In pharetra ac ligula ut posuere. Praesent viverra nunc ut ante fermentum mattis. Quisque eget metus porttitor velit mattis pretium. Morbi justo nulla, posuere condimentum vehicula quis, sollicitudin at turpis. Curabitur cursus, ligula in consectetur elementum, arcu odio vulputate odio, vitae vestibulum arcu mi ac lectus. Nam lacinia, lorem mollis lobortis gravida, sapien eros venenatis risus, a porttitor ex dui ac magna. </p>
         </div>
       </div>
     );
