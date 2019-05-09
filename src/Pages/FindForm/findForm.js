faf8ebac18842c8ac97940a788e2ee8b6c279f5a
Krzysztof Bortnowski <k.bortnowski91@gmail.com>
2019-05-09 22:20:22 +0200

    fixed forms

--- src/Pages/FindForm/findForm.js
+++ src/Pages/FindForm/findForm.js
@@ -2,31 +2,22 @@ import React, { Component } from 'react';
 import './findForm.css';
 import formularz from '../../img/LastForm/2.png';
 import zguba from '../../img/FindForm/4.PNG';
-import {storage, db } from '../../firebase';
+import { storage, db } from '../../firebase';
+import 'semantic-ui-css/semantic.min.css'
+import { Form, Input, TextArea } from 'semantic-ui-react';
 
 const red = {
   color: 'red',
 }
-const form= {
-  margin: '0 auto'
-}
-const secondFormLine ={
-  display:'block',
-  margin: 'auto',
-  padding:'5rem',
+const form = {
+  margin: '0 auto',
+  width: '1088px'
 }
 const label = {
-padding:'1rem',
-fontFamily: 'monospace',
-verticalAlign: 'middle'
-}
-const descriptionInput ={
-  height:'10rem',
-  padding:'2rem',
-  background:'transparent',
-  margin: '.5rem auto',
-  verticalAlign: 'middle'
+  width: '30rem',
 
+  fontFamily: "Lato,'Helvetica Neue',Arial,Helvetica,sans-serif",
+  verticalAlign: 'middle'
 }
 const lostStyle = {
   margin: '0 auto'
@@ -35,14 +26,6 @@ const formStyle = {
   margin: '40px auto',
   width: '20%',
 }
-const inputStyle = {
-  borderRadius: '1px',
-  height: '40px',
-  margin: '0 auto',
-  width:'200px',
-  boxShadow: '0 0 0 1px #000',
-  border: '5px solid transparent'
-}
 const uploader = {
   webkitAppearance: 'none',
   apperance: 'none',
@@ -50,32 +33,25 @@ const uploader = {
   marginBottom: '10px',
   color: 'white'
 }
-const formWrapper = {
-  margin: '0 auto',
-  background: '#F9F9F9'
-}
 const pictureWrapper = {
-  display:'flex',
-  flexDirection:'column',
-  justifyContent:'center',
-  alignItems:'center',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
 
 }
 const previewStyle = {
   maxHeight: "400px",
   maxWidth: "300px"
 }
-const imgStyle = {
-  maxHeight: "400px",
-  maxWidth: "400px"
-}
+
 
 
 class findForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      images:[],
+      images: [],
       progress: 0,
       image: null,
       url: '',
@@ -95,40 +71,40 @@ class findForm extends Component {
   handleChange = (event) => {
     this.setState({ [event.target.name]: event.target.value })
   }
-  handleFileInputChange  = (e)=> {
-    if(e.target.files[0]) {
-      const image = e.target.files[0] ;
-      this.setState(() => ({image}));
+  handleFileInputChange = (e) => {
+    if (e.target.files[0]) {
+      const image = e.target.files[0];
+      this.setState(() => ({ image }));
 
       e.preventDefault();
 
-  const uploadTask = storage.ref(`findImages/${image.name}`).put(image);
-  uploadTask.on('state_changed', (snapshot)=>{
-     //progress
-     const progress = Math.round((snapshot.bytesTransferred / snapshot.totalBytes)*100);
-     this.setState({progress});
-  }, (error) =>{
-    //error
-    console.log(error)
-  },
-   (complete) => {
-     //complete
-     storage.ref('findImages').child(image.name).getDownloadURL().then(url => {
-      console.log(url);
-      this.setState({url});
-     })
-   });
+      const uploadTask = storage.ref(`findImages/${image.name}`).put(image);
+      uploadTask.on('state_changed', (snapshot) => {
+        //progress
+        const progress = Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
+        this.setState({ progress });
+      }, (error) => {
+        //error
+        console.log(error)
+      },
+        (complete) => {
+          //complete
+          storage.ref('findImages').child(image.name).getDownloadURL().then(url => {
+            console.log(url);
+            this.setState({ url });
+          })
+        });
 
 
 
+    }
   }
-}
   handleSubmit(event) {
     alert('Ogłoszenie zostało dodane!');
     event.preventDefault();
     console.log(this.state)
     const findItemDetails = {
-      itemName : this.state.itemName,
+      itemName: this.state.itemName,
       email: this.state.email,
       phoneNumber: this.state.phoneNumber,
       location: this.state.location,
@@ -136,38 +112,26 @@ class findForm extends Component {
       description: this.state.description,
       url: this.state.url
     }
-    db.ref('/foundItem').push(findItemDetails, function(error){
+    db.ref('/foundItem').push(findItemDetails, function (error) {
       console.log(error)
       console.log('new item added');
-  });
+    });
   }
   render() {
- 
+
     return (
 
       <div className="form" style={form}>
         <img className="zguba" src={zguba} style={lostStyle} />
         <img className="formularz" src={formularz} style={formStyle} />
-        <form onSubmit={this.handleSubmit} style={formWrapper}>
-          <label style={label}>
-          Co znalazłeś?
-          </label>
-                  <input required type="text" name="itemName" value={this.state.itemName} onChange={this.handleChange} className="name" style={inputStyle} />
-         
-          <label style={label}>
-            Twój email:  
-            </label>
-        <input required type="email" name="email" value={this.state.email} onChange={this.handleChange} style={inputStyle} />
-      
-          <label style={label}>
-            Numer kontaktowy:
-            </label>
-            <input required type="tel" name="phoneNumber" pattern="[0-9]{3}-[0-9]{3}-[0-9]{3}" value={this.state.phoneNumber} onChange={this.handleChange} style={inputStyle} /><br />
-        
-          <div style={secondFormLine}>
-          <label style={label}>
-            Gdzie znalazłeś:</label>
-            <select required name="location" value={this.state.location} onChange={this.handleChange} style={inputStyle}>
+        <Form style={form} onSubmit={this.handleSubmit}>
+          <Form.Group widths='equal'>
+            <Form.Field control={Input} label='Co znalazłeś' placeholder='Co znalazłeś' required type="text" name="itemName" value={this.state.itemName} onChange={this.handleChange} className="name" />
+            <Form.Field control={Input} label='Twój email' placeholder='podajemail@mail.com' required type="email" name="email" value={this.state.email} onChange={this.handleChange} />
+            <Form.Field control={Input} label='Numer kontaktowy' placeholder='000-000-000' required type="tel" name="phoneNumber" pattern="[0-9]{3}-[0-9]{3}-[0-9]{3}" value={this.state.phoneNumber} onChange={this.handleChange} />
+          </Form.Group>
+          <Form.Group widths='equal'>
+            <Form.Field label='Gdzie znalazłeś' control='select' required>
               <option value="">Dzielnica</option>
               <option>Arkońskie-Niemierzyn</option>
               <option>Głębokie-Pilchowo</option>
@@ -206,11 +170,8 @@ class findForm extends Component {
               <option>Stołczyn</option>
               <option>Warszewo</option>
               <option>Żelechowa</option>
-            </select>
-         
-          <label style={label}>
-            Rodzaj przedmiotu:</label>
-            <select required name="category" value={this.state.category} onChange={this.handleChange} style={inputStyle}>
+            </Form.Field>
+            <Form.Field label='Co znalazłeś' control='select' required>
               <option value="">Kategoria</option>
               <option>Telefon</option>
               <option>Bizuteria</option>
@@ -219,23 +180,18 @@ class findForm extends Component {
               <option>Klucze</option>
               <option>Zwierze</option>
               <option>Inne</option>
-            </select>
-            <br /> <br />
-          <label style={label}>
-            Krótki opis znalezionej zguby:
-            </label>
-            <textarea required type="text" name="description" value={this.state.description} onChange={this.handleChange} rows="5" cols="50" style={descriptionInput} />
-            </div>  
+            </Form.Field>
+          </Form.Group>
+          <Form.Field control={TextArea} label='O przedmiocie' placeholder='Opisz znaleziony przedmiot' rows="5" cols="50" required type="text" name="description" value={this.state.description} onChange={this.handleChange} />
           <div style={pictureWrapper}>
-          <img src={this.state.url || 'https://via.placeholder.com/400x300'} alt="Uploaded images" style={previewStyle}/><br />
-          <input required type="file" onChange={this.handleFileInputChange}/><br /><br />
+            <img src={this.state.url || 'https://via.placeholder.com/400x300'} alt="Uploaded images" style={previewStyle} /><br />
+            <input required type="file" onChange={this.handleFileInputChange} /><br /><br />
 
-          <progress value={this.state.progress} max="100" style={uploader}></progress><br />
-          {(this.state.progress < 100)&&(this.state.progress  >0)&&<p style={red}>{`Proszę czekać... ${this.state.progress}%`}</p>}
-          <input type="submit" value="Dodaj zgłoszenie" style={label} />
-        </div>
-          
-        </form>
+            <progress value={this.state.progress} max="100" style={uploader}></progress><br />
+            {(this.state.progress < 100) && (this.state.progress > 0) && <p style={red}>{`Proszę czekać... ${this.state.progress}%`}</p>}
+            <input type="submit" value="Dodaj zgłoszenie" style={label} />
+          </div>
+        </Form>
       </div>
 
     );
