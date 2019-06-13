59ff0f7f0beb149f44362d204c36552ad9ee782e
Sonia <sonia@przenioslo.eu>
2019-06-13 13:23:00 +0200

    Update findDetail.js

--- src/Pages/FindDetail/findDetail.js
+++ src/Pages/FindDetail/findDetail.js
@@ -4,20 +4,55 @@ import './lost_find.css';
 import znalezione from "../../img/Home/5.png";
 import comeback from "../../img/FindDetail/2.png";
 import {Link} from 'react-router-dom';
+import { storage, db } from '../../firebase';
+
 
 class FindDetail extends Component {
+  state = { 
+    images: [],
+    itemName: '',
+    email: '',
+    phoneNumber: '',
+    location: '',
+    category: '',
+    description: '',
+   };
   render() {
+            
+            (e) => {
+            const storageRef = firebase.storage().ref("img/" + file.name);
+            const task = storageRef.get(file);
+            console.log(file);
+            task.on(
+              "state_changed",
+              () => {
+                storageRef.getDownloadURL().then(url => {
+                  this.setState({
+                    images: url
+                  });
+                });
+              }
+            );
+            const file = e.target.files[0];}
+            
+    
     return (
       <div className="App">
         <img className='title' src={znalezione}></img>
 
         <div className='lost_detail_container'>
         
-        <img className="lost-item-img" src={cat}></img>
+        <img className="lost-item-img" alt="" src={this.state.images}></img>
         
-        <div>ZGUBA</div>
-        <div>OPIS</div>
-        <div>KONTAKT</div>
+        
+        <div>{this.state.itemName}</div>
+        <div>{this.state.email}</div>
+        <div>{this.state.phoneNumber}</div>
+        <div>{this.state.location}</div>
+        <div>{this.state.category}</div>
+        <div>{this.state.description}</div>
+
+        
         </div>
         <Link className="more" to="/"><img className='title' src={comeback}></img></Link>
 
