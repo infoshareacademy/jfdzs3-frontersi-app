20f8b562e977fd0fe353cb9ecf1c5a7d8db488cb
Sonia <sonia@przenioslo.eu>
2019-06-14 17:59:55 +0200

    Update findDetail.js
    Added fetch

--- src/Pages/FindDetail/findDetail.js
+++ src/Pages/FindDetail/findDetail.js
@@ -10,19 +10,29 @@ import { storage, db } from '../../firebase';
 class FindDetail extends Component {
   state = { 
     foundItems: {},
-    lostItems: {},
-
+    itemName: '',
+    email: '',
+    phoneNumber: '',
+    location: '',
+    category: '',
+    description: '',
+    image: [],
    };
    
    componentDidMount(){
     fetch(`https://zgubionepl.firebaseio.com/foundItem.json/${this.props.match.params.id}`)
-    .then(response => response.json())
-    .then(result => {
-        this.setState({
-            foundItems: result,
-        })
-    })
-}
+      .then(response => response.json())
+      .then(result => {
+        const itemKey = Object.keys(result)[0];
+        console.log(itemKey);
+        this.setState({ foundItem: result,
+        loading : false})
+     })
+      .then(foundItem => {
+        this.setState({foundItem})
+      })
+     ;
+  }
   render() {
     
     return (
@@ -33,7 +43,7 @@ class FindDetail extends Component {
         
         <img className="lost-item-img" alt="" src={this.state.images}></img>
         
-        <div>{this.state.foundItems.itemName}</div>
+        <div>{this.state.foundItem}</div>
         <div>{this.state.email}</div>
         <div>{this.state.phoneNumber}</div>
         <div>{this.state.location}</div>
